ad74add42d652636dd3e91d220c222600e759272
Arvish Pandey <96754932+arvish@users.noreply.github.com>
2022-06-25 13:20:08 -0400

    Added final python codes

---
+++ Proj_Python_forDS_Code.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Proj_Python_forDS_Code.py
+@Time    :   2022/06/25 13:11:00
+@Author  :   Arvish Pandey 
+@Version :   1.0
+@Contact :   arvishpandey@icloud.com
+@License :   (C)Copyright 2022, Apache License 2.0
+@Desc    :   Python Codes for Project Extracting Stock Revenue using Python Libraries & WebScraping Methods
+'''
+
+# here put the import lib
+
+# pip install yfinance==0.1.67
+# pip install pandas==1.3.3
+# pip install requests==2.26.0
+# mamba install bs4 -y
+# pip install plotly==5.3.1
+
+# Making sure these libraries are intalled before running the codes
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+# pip install pandas==1.3.3
+# pip install requests==2.26.0
+# pip install bs4 
+# pip install html5lib
+# pip install lxml==4.6.4
+# pip install plotly==5.3.1
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    stock_data_specific = stock_data[stock_data.Date <= '2021--06-14']
+    revenue_data_specific = revenue_data[revenue_data.Date <= '2021-04-30']
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data_specific.Date, infer_datetime_format=True), y=stock_data_specific.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data_specific.Date, infer_datetime_format=True), y=revenue_data_specific.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+
+tesla = yf.Ticker("TSLA")
+tesla_data = tesla.history(period="max")
+tesla_data.reset_index(inplace=True)
+tesla_data.head(n=5)
+
+url = "https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue"
+html_data  = requests.get(url).text
+
+soup = BeautifulSoup(html_data, 'html5lib')
+
+tesla_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+for table in soup.find_all('table'):
+
+    if ('Tesla Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        for row in rows:
+            col = row.find_all('td')
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text
+                tesla_revenue = tesla_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+
+tesla_revenue["Revenue"] = tesla_revenue['Revenue'].str.replace(',|\$',"")
+
+tesla_revenue.dropna(inplace=True)
+tesla_revenue = tesla_revenue[tesla_revenue['Revenue'] != ""]
+
+tesla_revenue.tail(n=5)
+
+GameStop = yf.Ticker("GME")
+gme_data = GameStop.history(period="max")
+gme_data.reset_index(inplace=True)
+gme_data.head(n=5)
+
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0220EN-SkillsNetwork/labs/project/stock.html"
+
+data = requests.get(url).text
+
+soup = BeautifulSoup(data, "html5lib")
+
+gme_revenue = pd.DataFrame(columns=['Date', 'Revenue'])
+for table in soup.find_all('table'):
+    if ('GameStop Quarterly Revenue' in table.find('th').text):
+        rows = table.find_all('tr')
+        for row in rows:
+            col = row.find_all('td')
+            if col != []:
+                date = col[0].text
+                revenue = col[1].text.replace(',',"").replace('$',"")
+                gme_revenue = gme_revenue.append({"Date":date, "Revenue":revenue}, ignore_index=True)
+            
+gme_revenue.tail(n=5)
+
+
+make_graph(tesla_data, tesla_revenue, 'Tesla')
+
+make_graph(gme_data, gme_revenue, 'GameStop')
